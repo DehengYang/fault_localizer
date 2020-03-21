e115f36b8b17cbc7c43b8b5169c5c4c98b02c082
DehengYang <1660542154@qq.com>
2020-03-21 03:06:51 -0700

    some improvements for varNode class

--- apr/src/main/java/apr/apr/repair/parser/VariableNode.java
+++ apr/src/main/java/apr/apr/repair/parser/VariableNode.java
@@ -3,6 +3,9 @@
  */
 package apr.apr.repair.parser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.javaparser.ast.Node;
 
 /**
@@ -11,10 +14,15 @@ import com.github.javaparser.ast.Node;
  *
  */
 public class VariableNode {
+	final static Logger logger = LoggerFactory.getLogger(VariableNode.class);
+	
 	private Node node;
 	private String varName;
 	private String varOriType;
 	private String varRefType;
+	
+	private int beginLineNo;
+	private int endLineNo;
 	
 	public VariableNode(Node node, String varName, String varOriType){
 		this(node, varName, varOriType, "");
@@ -25,6 +33,9 @@ public class VariableNode {
 		this.setVarName(varName);
 		this.setVarOriType(varOriType);
 		this.setVarRefType(varRefType);
+		
+		beginLineNo = node.getRange().get().begin.line;
+		endLineNo = node.getRange().get().end.line;
 	}
 
 	/**
@@ -36,18 +47,41 @@ public class VariableNode {
 	 */
 	@Override
 	public String toString(){
-		return String.format("Variable: %s, name: %s, oriType: %s, refType: %s, range: %s-%s", node.toString(), varName, varOriType, varRefType,
-				node.getRange().get().begin.line, node.getRange().get().end.line);
+		return String.format("Variable: %s, name: %s, oriType: %s, refType: %s, range: %s-%s, class: %s", node.toString(), varName, varOriType, varRefType,
+				beginLineNo, endLineNo, node.getClass());
 	}
 	
 	@Override
 	public boolean equals(Object obj){
 		if (obj instanceof VariableNode){
 			VariableNode vn = (VariableNode) obj;
-			return node.equals(vn.getNode());
+			boolean equal = node.equals(vn.getNode());
+			if (equal){
+				logger.debug("repeated info: {}, {}, {}, {}.",this.varName, vn.getVarName(), this.beginLineNo, vn.getBeginLineNo());
+				// may not be the totoal same location node.
+				//2020-03-21 01:07:10 DEBUG VariableNode:60 - repeated info: t, t, 144, 113.
+			}
+			return equal;
 		}
 		return false;
 	}
+	
+	public int getBeginLineNo() {
+		return beginLineNo;
+	}
+
+	public void setBeginLineNo(int beginLineNo) {
+		this.beginLineNo = beginLineNo;
+	}
+	
+	public int getEndLineNo() {
+		return endLineNo;
+	}
+
+	public void setEndLineNo(int endLineNo) {
+		this.endLineNo = endLineNo;
+	}
+
 	
 	public Node getNode() {
 		return node;
