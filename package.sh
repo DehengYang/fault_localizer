2fbe9c7248b6573df3fb2f4c8abb179df8db8231
DehengYang <1660542154@qq.com>
2022-04-14 15:59:49 +0800

    update package.sh

--- package.sh
+++ package.sh
@@ -4,6 +4,8 @@ java -version
 cd -
 mvn clean package -DskipTests
 
+[ ! -d "versions" ] && echo "mkdir versions" && mkdir versions
+
 cp target/gzoltar_localizer-0.0.1-SNAPSHOT-jar-with-dependencies.jar versions/
 cd ~/env
 ./change-jdk-version.sh 8
